997873223775857744a92180ed7aeadedc497b06
Chuck McAndrew <chuck.mcandrew@leblibrary.com>
2020-05-07 12:52:09 -0400

    refactor pdf.py to work with aws lamdba

--- pdf.py
+++ pdf.py
@@ -1,13 +1,18 @@
-from fpdf import FPDF
+import datetime
 import barcode
+import random
+import json
+from fpdf import FPDF
 from barcode.writer import ImageWriter
 
 class Citizen():
-    def __init__(self, name, expiration, city, barcode):
+    def __init__(self, name, city, email):
         self.name = name
-        self.expiration = expiration
         self.city = city
-        self.barcode = barcode
+        self.email = email
+        expiration = datetime.datetime.now() + datetime.timedelta(days=365)
+        self.expiration = expiration.date().strftime("%m-%d-%Y") 
+        self.barcode = str(random.randint(99999999999,1000000000000))
 
 
 class CustomPDF(FPDF):
@@ -24,14 +29,23 @@ class CustomPDF(FPDF):
         # Line break
         self.ln(20)
 
+def permit(event, context):
+    body = json.loads(event['body'])
+    name = body['name']
+    email = body['email']
+    city = body['city']
+    citizen = Citizen(name, city, email)
+    pdf_path = create_pdf(citizen)
+    msg = 'Created permit for {}, with barcode number {} and expiration of {} and emailed it to {}'.format(name, citizen.barcode, citizen.expiration, email)
+    return json.dumps({"message": msg})
 
-def create_pdf(pdf_path, citizen):
+
+def create_pdf(citizen):
     pdf = CustomPDF()
     # Create the special value {nb}
     pdf.alias_nb_pages()
     pdf.add_page(orientation='P')
     pdf.set_font('Times', '', 36)
-    line_no = 1
     pdf.image(create_barcode(citizen.barcode), 8, 55, 200)
     pdf.ln(110)
     pdf.cell(20)
@@ -40,13 +54,14 @@ def create_pdf(pdf_path, citizen):
     pdf.cell(0, 20, txt="Expiration Date: {}".format(citizen.expiration), ln=1)
     pdf.cell(20)
     pdf.cell(0, 20, txt="City of Origin: {}".format(citizen.city), ln=1)
-    pdf.output(pdf_path)
+    pdf.output("permit.pdf")
+    return "permit.pdf"
 
 def create_barcode(code):
     ean = barcode.get('ean13', code, writer=ImageWriter())
-    filename = ean.save(code)
+    filename = ean.save(barcode)
     return filename
 
 if __name__ == '__main__':
-    citizen = Citizen("Joe Blow", "05/05/2020", "Lebanon", "123456789012")
+    citizen = Citizen("Joe Blow", "05/05/2020", "Lebanon", "123456789101","example@example.com")
     create_pdf('permit.pdf', citizen)
